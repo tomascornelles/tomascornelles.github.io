52c9f0e9c1859c81500281aef560815f99c52f37
Tomas Cornelles <menos5@gmail.com>
2023-08-02 22:37:09 +0200

    ADD Baseline tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('G001 - Baseline', () => {
+  it('Load home', () => {
     cy.visit('http://localhost:8080')
+  })
+
+  it('Load 404', () => {
+    cy.visit('http://localhost:8080/kjhfdgoid')
   })
 })
