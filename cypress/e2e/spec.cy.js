03af688f20e4313f7518397e47cecd0f4d89359d
Tomas Cornelles <menos5@gmail.com>
2023-08-03 13:30:20 +0200

    DELETE 11ty tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 describe('G001 - Baseline', () => {
-  it('Load home', () => {
-    cy.visit('http://localhost:8080')
-    cy.get('h2').contains('WebC')
-  })
+  // it('Load home', () => {
+  //   cy.visit('http://localhost:8080')
+  //   cy.get('h2').contains('WebC')
+  // })
 
-  it('Load 404', () => {
-    cy.visit('http://localhost:8080/404')
-  })
+  // it('Load 404', () => {
+  //   cy.visit('http://localhost:8080/404')
+  // })
 })
