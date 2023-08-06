def0d470da9bbc6ff3eebde0873d709ef513d366
Tomas Cornelles <menos5@gmail.com>
2023-08-06 21:17:49 +0200

    ADD favicon

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,8 @@
     Home
   </Title>
 
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>">
+
   <Header />
   <h1>
     Nuxt way
