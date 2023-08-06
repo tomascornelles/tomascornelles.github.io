f16b9622bd0e5a1e3828c8c4459657cd97e70ce2
Tomas Cornelles <menos5@gmail.com>
2023-08-06 21:53:14 +0200

    ADD 11ty structure

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
+module.exports = function(eleventyConfig) {
+  // Add support for WebC
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "../_includes/layouts/**/*.webc"
+  });
+
+  eleventyConfig.addPassthroughCopy({ "_includes/assets": "assets" });
+
+  eleventyConfig.setServerOptions({
+    watch: ['docs/**/*.*'],
+  });
+
+  // Return your Object options:
+  return {
+    dir: {
+      includes: "../_includes",
+      layouts: "../_includes/layouts",
+      input: "_views",
+      output: "docs" // hack for github pages
+    }
+  }
+};
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,10 @@
+describe('G001 - Baseline', () => {
+  it('Load home', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('h2').contains('WebC')
+  })
+
+  it('Load 404', () => {
+    cy.visit('http://localhost:8080/404')
+  })
+})
