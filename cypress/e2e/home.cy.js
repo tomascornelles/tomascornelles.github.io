f217967815659ecd98af47777cfcdc26e3171951
Tomás Cornelles <tomascornelles@gmail.com>
2023-10-28 22:00:52 +0200

    FIX home tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,16 +1,16 @@
 describe('G003 - Home', () => {
   it('Post title', () => {
     cy.visit('http://localhost:8080');
-    cy.get('#blog h2 a').click();
+    cy.get('#blog h2 a').click({force: true});
   });
 
   it('Post read more', () => {
     cy.visit('http://localhost:8080');
-    cy.contains('Continúa leyendo').click();
+    cy.contains('Continúa leyendo').click({force: true});
   });
 
   it('Blog link', () => {
     cy.visit('http://localhost:8080');
-    cy.contains('Leer más artículos en el blog').click();
+    cy.contains('Leer más artículos en el blog').click({force: true});
   });
 })
